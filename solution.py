f4e1cdd814136276e94c10a19d082c39116bf2bf
Vladislav K <110909601+ch1kim0n1@users.noreply.github.com>
2026-03-19 21:55:59 -0500

    Create solution.py

---
+++ solution.py
@@ -0,0 +1,11 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+from typing import List, Optional
+
+class Solution:
+    def sherlockSort(self, nums: List[int]) -> List[int]:
+        pass
